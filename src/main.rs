a833b5a15466e4e11fff672daadd87f76f5b149a
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-08-29 02:08:53 -0400

    Add directx .x sub system

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate pyramid;
 extern crate pyramid_animation;
 extern crate pyramid_viewport;
 extern crate pyramid_template;
+extern crate pyramid_legacy_directx_x_loader;
 
 use std::path::Path;
 
@@ -15,6 +16,7 @@ fn main() {
     let root_path = path.parent().unwrap().to_path_buf();
     let mut system = System::new();
     system.add_subsystem(Box::new(pyramid_template::TemplateSubSystem::new(root_path.clone())));
+    system.add_subsystem(Box::new(pyramid_legacy_directx_x_loader::LegacyDirectXXSubSystem::new(root_path.clone())));
     system.add_subsystem(Box::new(pyramid_animation::AnimationSubSystem::new()));
     system.add_subsystem(Box::new(pyramid_viewport::ViewportSubSystem::new(root_path.clone())));
     system.set_document(doc);
