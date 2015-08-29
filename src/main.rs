a68eedcea36360f9d5cb49c217def8885537b5f5
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-08-28 22:43:10 -0400

    Add templating sub system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate pyramid;
 extern crate pyramid_animation;
 extern crate pyramid_viewport;
+extern crate pyramid_template;
 
 use std::path::Path;
 
@@ -9,11 +10,13 @@ use pyramid::system::*;
 
 fn main() {
 
-    let path = Path::new("../examples/test.pml");
+    let path = Path::new("../examples/dml_level_a.pml");
     let doc = Document::from_file(path);
+    let root_path = path.parent().unwrap().to_path_buf();
     let mut system = System::new();
+    system.add_subsystem(Box::new(pyramid_template::TemplateSubSystem::new(root_path.clone())));
     system.add_subsystem(Box::new(pyramid_animation::AnimationSubSystem::new()));
-    system.add_subsystem(Box::new(pyramid_viewport::ViewportSubSystem::new(path.parent().unwrap().to_path_buf())));
+    system.add_subsystem(Box::new(pyramid_viewport::ViewportSubSystem::new(root_path.clone())));
     system.set_document(doc);
 
     println!("Starting main loop");
