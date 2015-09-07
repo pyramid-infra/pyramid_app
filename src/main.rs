b92583f15471cbd14eac70ada5ca2ccca58eb69c
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-09-07 14:09:03 -0400

    Add pyramid_transform

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate pyramid_animation;
 extern crate pyramid_viewport;
 extern crate pyramid_template;
 extern crate pyramid_legacy_directx_x_loader;
+extern crate pyramid_transform;
 
 use std::path::Path;
 use std::fs::File;
@@ -13,13 +14,14 @@ use pyramid::system::*;
 
 fn main() {
 
-    let path = Path::new("../examples/test.pml");
+    let path = Path::new("../examples/levela/map.pml");
     let doc = Document::from_file(path);
     let root_path = path.parent().unwrap().to_path_buf();
     let mut system = System::new();
     system.add_subsystem(Box::new(pyramid_template::TemplateSubSystem::new(root_path.clone())));
     system.add_subsystem(Box::new(pyramid_legacy_directx_x_loader::LegacyDirectXXSubSystem::new(root_path.clone())));
     system.add_subsystem(Box::new(pyramid_animation::AnimationSubSystem::new()));
+    system.add_subsystem(Box::new(pyramid_transform::TransformSubSystem::new()));
     system.add_subsystem(Box::new(pyramid_viewport::ViewportSubSystem::new(root_path.clone())));
     system.set_document(doc);
 
